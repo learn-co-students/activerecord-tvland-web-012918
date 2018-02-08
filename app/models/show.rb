72704db8ad728b378f0d526e4d20f6c49761ac11
github username <github email address>
2018-02-08 10:28:44 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(network)
+    new_net = Network.new(network)
+    self.network = new_net
+  end
 end
