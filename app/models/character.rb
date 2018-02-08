38c1efc941e9bfb5a8ecfb761a72d92d6387530f
brookescarlett <brooke.yalof2017@u.northwestern.edu>
2018-02-08 10:49:34 -0500

    done

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  # def actor
+  #   binding.pry
+  #   # self.actor
+  # end
+
+
 end
