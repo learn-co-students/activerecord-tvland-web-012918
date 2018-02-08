a18fc32ca0227d4b300f7cb0332c7c21b89b4d7a
gilsunshine <gilsunshine@gmail.com>
2018-02-08 08:54:34 -0500

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def self.build_show(character)
+    self.show.characters << character
+  end
+
 end
---
+++ db/migrate/008_add_show_attr.rb
@@ -0,0 +1,6 @@
+class AddShowAttr < ActiveRecord::Migration
+  def change
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
+  end
+end
